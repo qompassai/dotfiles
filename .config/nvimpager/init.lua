a822c2d411363963d0909a538f86698574ee6f33
Matt A. Porter <map@qompass.ai>
2025-05-26 20:37:43 -0700

    update

---
+++ .config/nvimpager/init.lua
@@ -0,0 +1,10 @@
+-- /qompassai/dotfiles/.config/nvimpager/init.lua
+-- ----------------------------
+-- Copyright (C) 2025 Qompass AI, All rights reserved
+nvimpager.follow = false
+nvimpager.maps = true
+vim.opt.loadplugins = false
+vim.opt.foldmethod = "syntax"
+vim.opt.foldlevel = 99
+vim.opt.lazyredraw = true
+vim.opt.synmaxcol = 200
