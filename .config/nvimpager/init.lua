4cfadb26eb2c761c3d2dfa88d674c592e97ad95a
phaedrusflow <map@qompass.ai>
2025-09-01 17:00:34 -0700

    update

--- .config/nvimpager/init.lua
+++ .config/nvimpager/init.lua
@@ -1,9 +1,10 @@
 -- /qompassai/dotfiles/.config/nvimpager/init.lua
--- ----------------------------
+-- Qompass AI Nvimpager Config
 -- Copyright (C) 2025 Qompass AI, All rights reserved
-nvimpager.follow = false
+-----------------------------------------------------
+nvimpager.follow = true
 nvimpager.maps = true
-vim.opt.loadplugins = false
+vim.opt.loadplugins = true
 vim.opt.foldmethod = "syntax"
 vim.opt.foldlevel = 99
 vim.opt.lazyredraw = true
--- .config/zig/zenv.sh
+++ .config/zig/zenv.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
-# ~/.config/zig/zenv.sh
+#!/usr/bin/env bash
+# /qompassai/dotfiles/.config/zig/zenv.sh
+# # Qompass AI Zig Env Config
+# Copyright (C) 2025 Qompass AI, All rights reserved
+####################################################
+
 export ZIG_VERBOSE_CC=1
 export ZIG_BTRFS_WORKAROUND=1
