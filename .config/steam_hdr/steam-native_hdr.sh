abfa8aee32920b029938034860afcde179818461
phaedrusflow <map@qompass.ai>
2025-11-06 17:24:55 -0800

    update

---
+++ .config/steam_hdr/steam-native_hdr.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# /qompassai/dotfiles/.config/steam_hdr/steam-native_hdr.sh
+# Qompass AI Steam Native HDR Script
+# Copyright (C) 2025 Qompass AI, All rights reserved
+############################################################
+ENABLE_HDR_WSI=1 gamescope --fullscreen -w 2560 -h 1440 --hdr-enabled --hdr-debug-force-output --steam -- env ENABLE_GAMESCOPE_WSI=1 DXVK_HDR=1 DISABLE_HDR_WSI=1 steam-native -bigpicture
