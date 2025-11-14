c91ba13cbc47b8d4dc28405fbc58cc928f7bbe7f
phaedrusflow <map@qompass.ai>
2025-11-13 16:19:39 -0800

    update

---
+++ .config/wifite/audit.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# /qompassai/dotfiles/.config/wifit/audit.sh
+# Qompass AI Wifite Audit
+# Copyright (C) 2025 Qompass AI, All rights reserved
+####################################################
+wifite --wps --power 45 --bully --wps-only --inf --daemon
