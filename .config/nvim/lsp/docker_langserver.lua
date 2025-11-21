519ab4a3e6564ea16d5933cb2c1f44ffa4661541
phaedrusflow <map@qompass.ai>
2025-11-20 17:13:41 -0800

    update

---
+++ .config/nvim/lsp/docker_langserver.lua
@@ -0,0 +1,28 @@
+-- docker_langserver.lua
+-- Qompass AI - [Add description here]
+-- Copyright (C) 2025 Qompass AI, All rights reserved
+-- ----------------------------------------
+-- go install github.com/docker/docker-language-server/cmd/docker-language-server@latest
+---@type vim.lsp.Config
+return {
+  cmd = { 'docker-language-server', 'start', '--stdio' },
+  filetypes = { 'dockerfile', 'yaml.docker-compose' },
+  get_language_id = function(_, ftype)
+    if ftype == 'yaml.docker-compose' or ftype:lower():find('ya?ml') then
+      return 'dockercompose'
+    else
+      return ftype
+    end
+  end,
+  root_markers = {
+    'Dockerfile',
+    'docker-compose.yaml',
+    'docker-compose.yml',
+    'compose.yaml',
+    'compose.yml',
+    'docker-bake.json',
+    'docker-bake.hcl',
+    'docker-bake.override.json',
+    'docker-bake.override.hcl',
+  },
+}
