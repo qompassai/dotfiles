29c2521b8cf3c8e0e4e533a9edfeae9ae05c6b64
Matt Porter <map@qompass.ai>
2026-06-30 13:12:34 -0700

    update

--- .config/eepm/play.d/zoom.sh
+++ .config/eepm/play.d/zoom.sh
@@ -1,25 +1,25 @@
-#!/bin/sh
-
+#!/usr/bin/env bash
 PKGNAME=zoom
 SUPPORTEDARCHES="x86_64 x86"
 VERSION="$2"
 DESCRIPTION="Zoom client from the official site"
 URL="https://zoom.us"
-
-. $(dirname $0)/common.sh
+. "$(dirname "$0")"/common.sh
 
 [ "$VERSION" = "*" ] && VERSION="latest"
 
 arch=$(epm print info --distro-arch)
 case $arch in
-    x86_64|amd64)
-        arch=$arch ;;
-    i586|i386)
-        # latest does not work
+    x86_64 | amd64)
+        arch="$arch"
+        ;;
+    i586 | i386)
         VERSION=5.4.53391.1108
-        arch=i686 ;;
+        arch=i686
+        ;;
     *)
         fatal "Unsupported arch $arch for $(epm print info -d)"
+        ;;
 esac
 
 # TODO: there are more complex distro dependent url
